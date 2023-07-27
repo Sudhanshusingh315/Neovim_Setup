48a1dab80984e2440961b420009d78b2fc865e8e
Sudhanshu singh <ss826063@gmail.com>
2023-07-27 20:52:24 +0530

    initial commit

---
+++ init.lua
@@ -0,0 +1,18 @@
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+--requi-jhhh-[[ - ]][[ r ]]e "user.colorscheme"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.autopairs"
+require "user.comment"
+require "user.gitsign"
+require "user.nvim-tree"
+require "user.bufferline" 
+
+--forching a cholorscheme
+vim.cmd "colorscheme gruvbox"
+
+
